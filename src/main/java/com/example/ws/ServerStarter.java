38d71baa4cf63a40e2c43cad5c4fac711fcbfd13
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2013-12-03 16:57:20 -0700

    DefaultServlet must be named 'default'

--- src/main/java/com/example/ws/ServerStarter.java
+++ src/main/java/com/example/ws/ServerStarter.java
@@ -12,21 +12,24 @@ import com.example.config.AppConfig;
 
 public class ServerStarter  {
     public static void main( String[] args ) throws Exception {
-		Server server = new Server( 8080 );
-        
-		// Create the 'root' Spring application context
- 		final ServletHolder servletHolder = new ServletHolder( new DefaultServlet() );
- 		final ServletContextHandler context = new ServletContextHandler();
+        Server server = new Server(8080);
 
- 		context.setContextPath( "/" );
- 		context.addServlet( servletHolder, "/*" );
- 		context.addEventListener( new ContextLoaderListener() ); 		
- 		context.setInitParameter( "contextClass", AnnotationConfigWebApplicationContext.class.getName() );
- 		context.setInitParameter( "contextConfigLocation", AppConfig.class.getName() );
- 		
-        server.setHandler( context );
-        WebSocketServerContainerInitializer.configureContext( context );        
-        
+        // Create the 'root' Spring application context
+        final ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        context.addEventListener(new ContextLoaderListener());
+        context.setInitParameter("contextClass",AnnotationConfigWebApplicationContext.class.getName());
+        context.setInitParameter("contextConfigLocation",AppConfig.class.getName());
+
+        // Create default servlet (servlet api required)
+        String resourceBase = System.getProperty("user.dir");
+        final ServletHolder defaultHolder = new ServletHolder("default",DefaultServlet.class);
+        defaultHolder.setInitParameter("resourceBase",resourceBase);
+        context.addServlet(defaultHolder,"/");
+
+        server.setHandler(context);
+        WebSocketServerContainerInitializer.configureContext(context);
+
         server.start();
         server.join();	
     }
